6a05ba4d7e357dd437acae687944014f1defaf2f
pampers <pepperoni9@gmail.com>
2017-08-02 16:41:26 +0900

    create a base layout

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+
 export class App {
-  message = 'Hello World!';
+
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+
+    config.title = 'Learning Aurelia';
+    config.options.pushState = true;
+    config.options.hasChange = false;
+    config.map([
+      { route: '', redirect: 'contacts' },
+      { route: 'contacts', name: 'contacts', moduleId: 'contact-list', nav: true, title: 'Contacts' },
+      { route: 'contacts/:id', name: 'contact-details', moduleId: 'contact-details' }
+    ]);
+    config.mapUnknownRoutes('not-found');
+
+    this.router = router;
+  }
 }
